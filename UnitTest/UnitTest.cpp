7aa5c0b973e35c058ff17271c8a10a1e974aab22
Demian Rymar <ua.demyan@gmail.com>
2024-04-25 19:26:49 +0300

    Add project files.

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LAB_12.2_ITER/Main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+
+		TEST_METHOD(hasDuplicatePairTest)
+		{
+			Node* head = nullptr;
+			Node* tail = nullptr;
+			int values[] = { 1, 2, 3, 4, 5, 2 };
+			int size = sizeof(values) / sizeof(values[0]);
+
+			createList(head, tail, values, size);
+
+			bool result = hasDuplicatePair(head);
+
+			Assert::IsTrue(result);
+
+			Node* current = head;
+			while (current != nullptr) {
+				Node* temp = current;
+				current = current->next;
+				delete temp;
+			}
+		}
+	};
+}
